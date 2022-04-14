c40c21f1c964916c247dc3fc1ac327b13fcb9fff
hemam5526 <72795011+hemam5526@users.noreply.github.com>
2022-04-14 15:20:08 +0530

    Level Win trigger
    created the door, tag on the door, created player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal")>0)
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }
+        if(Input.GetAxis("Horizontal")<0)
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
+        }
+        if(Input.GetAxis("Vertical")>0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, speed);
+        }
+        if(Input.GetAxis("Vertical")<0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }
+        else if(Input.GetAxis("Vertical")==0 && Input.GetAxis("Horizontal")==0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Door")
+        {
+            Debug.Log("Level Completed !!!");
+        }
+        
+    }
+}
